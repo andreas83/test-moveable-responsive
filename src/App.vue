eed504f595a7b842af93b41abb40b4247e94bc39
andreas <andreas@moving-bytes.at>
2024-12-12 13:18:07 +0100

    Set transform-origin to top left which fixes the offset issue (thx to @neuron303) some smaller cleanup added

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,6 @@
     
     <div class="debug">
       <button @click="$refs.target.style.transform = ' translate(0px, 0px) rotate(0deg) scale(1, 1)'">reset</button>
-      <button @click="moveLeft(50)">move 50px left from current position</button>
       <button @click="$refs.container.style.width = '25%'">set container width to 50%</button>
       <button @click="$refs.container.style.width = '50%'">set container width to 100%</button>
 
@@ -38,6 +37,7 @@
   width: 50px;
   height: 50px;
   background-color: red;
+  transform-origin: top left;
 }
 
 .debug {
@@ -62,7 +62,7 @@
 }
 </style>
 <script setup>
-import { computed, onMounted, ref } from "vue";
+import {  onMounted, ref } from "vue";
 import Moveable from "vue3-moveable";
 
 const target = ref(null);
@@ -92,19 +92,20 @@ onMounted(() => {
   targetOrginalTransform.value = target.value.style.transform;
   previousContainerWidth.value = containerOriginalDimension.value.width;
 
-  let isInit = true;
+
 
   const resizeObserver = new ResizeObserver((entries) => {
     const { width } = entries[0].contentRect;
-    if(isInit){
-      isInit = false;
-      return;
-    }
+
     // Calculate ratio based on previous width instead of original width
 
     // Get current transform values
     const transform = document.getElementById("target").style.transform;
+
+    // TODO only parse when attribute changed
     
+
+
     // Get current translate values
     const translateMatch = transform.match(/translate\(([^)]+)\)/);
     const translateValues = translateMatch[1].split(",");
@@ -131,51 +132,21 @@ onMounted(() => {
     const newScaleY = currentScaleY * widthRatio;
     const newTransX = currentTransX * widthRatio;
     const newTransY = currentTransY * widthRatio;
-
+    
     // Apply the new transform
-    document.getElementById("target").style.transform = 
-      `translate(${newTransX}px, ${newTransY}px) rotate(${rotateValue}) scale(${newScaleX}, ${newScaleY})`;
+    const transString = `translate(${newTransX}px, ${newTransY}px) rotate(${rotateValue}) scale(${newScaleX}, ${newScaleY})`;
+    document.getElementById("target").style.transform = transString;
 
-      previousContainerWidth.value = width;
+    previousContainerWidth.value = width;
 
     // Update moveable rect
     moveable.value.updateRect();
 });
   resizeObserver.observe(document.getElementById("container"))
-
-
-
 })
 
 
-computed(() => {
-  transform : () => {
-    return target.value.style.transform
-  }
-})
 
-const moveLeft = (px) => {
-  const transform = target.value.style.transform;
 
-  
-  const match = transform.match(/(translate\(([^)]+)\))/);
-  const values = match[2].split(",");
-  const currentTransX = parseFloat(values[0]);
-  const currentTransY = parseFloat(values[1]);
-  const newTransX = currentTransX - px;
-  const newTransY = currentTransY;
 
-  const scale = target.value.style.transform.match(/scale\(([^)]+)\)/);
-  const scaleValues = scale[1].split(",");
-
-  const rotate = target.value.style.transform.match(/rotate\(([^)]+)\)/);
-  let rotateValues = rotate[1].split(",") || [0];
-
-  
-  target.value.style.transform = `translate(${newTransX}px, ${newTransY}px) rotate(${rotateValues[0]}) scale(${scaleValues[0]}, ${scaleValues[1]})`;
-
-  moveable.value.updateRect();
-
-};
 </script>
-
